7632143bf3814fab43fbf1b229b463b988ae4a22
Lindsey Liu <lindseyl@yahoo-inc.com>
2017-01-25 15:31:00 -0800

    login screen

--- app/src/main/java/com/codepath/codepathlab1/SampleFragmentPagerAdapter.java
+++ app/src/main/java/com/codepath/codepathlab1/SampleFragmentPagerAdapter.java
@@ -11,7 +11,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
     final int PAGE_COUNT = 2;
-    private String tabTitles[] = new String[] { "Sign Up", "Log In" };
+    private String tabTitles[] = new String[]{"Sign Up", "Log In"};
     private Context context;
 
     public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
